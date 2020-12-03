81ef7f0ba6edac9a9e3ccbc6e836ac3617bdf408
ccaillol <ccaillol@cern.ch>
2020-12-03 15:50:45 +0100

    add looser_lep shape in etau

--- bin/SMHTT2016_et.cpp
+++ bin/SMHTT2016_et.cpp
@@ -472,7 +472,8 @@ vector<string> ggH_STXS;
   cb.cp().process(JoinStr({{"ZT","TTT","VVT","STT","ZL","TTL","VVL","STL","ggH_hww125","qqH_hww125","WH_hww125","ZH_hww125","OutsideAcceptance"},sig_procs})).AddSyst(cb,"CMS_eff_e_2016","lnN",SystMap<>::init(1.02));
 
   // Against ele and against mu for real taus
-  cb.cp().process(JoinStr({{"ZT","TTT","VVT","STT","ggH_hww125","qqH_hww125","WH_hww125","ZH_hww125","OutsideAcceptance"},sig_procs})).AddSyst(cb,"CMS_eff_t_againstemu_et_2016","lnN",SystMap<>::init(1.03));
+  //cb.cp().process(JoinStr({{"ZT","TTT","VVT","STT","ggH_hww125","qqH_hww125","WH_hww125","ZH_hww125","OutsideAcceptance"},sig_procs})).AddSyst(cb,"CMS_eff_t_againstemu_et_2016","lnN",SystMap<>::init(1.03));
+    cb.cp().process(JoinStr({{"ZT","TTT","VVT","STT","ggH_hww125","qqH_hww125","WH_hww125","ZH_hww125","OutsideAcceptance"},sig_procs})).AddSyst(cb,"CMS_looser_lep_wp_etau", "shape", SystMap<>::init(1.00));
 
   // Trg efficiency. Can be a single lnN because only single ele trigger
   cb.cp().process(JoinStr({{"ZT","TTT","VVT","STT","ZL","TTL","VVL","STL","ggH_hww125","qqH_hww125","WH_hww125","ZH_hww125","OutsideAcceptance"},sig_procs})).AddSyst(cb,"CMS_singleeletrg_2016","lnN",SystMap<>::init(1.02));
@@ -1092,7 +1093,8 @@ vector<string> ggH_STXS;
       cb.cp().process({"embedded"}).AddSyst(cb,"CMS_eff_t_embedded_ptgt40_2016", "shape", SystMap<>::init(1.00));
 
       // Against ele and against mu for real taus
-      cb.cp().process({"embedded"}).AddSyst(cb,"CMS_eff_t_againstemu_embedded_et_2016","lnN",SystMap<>::init(1.05));
+      //cb.cp().process({"embedded"}).AddSyst(cb,"CMS_eff_t_againstemu_embedded_et_2016","lnN",SystMap<>::init(1.05));
+      cb.cp().process({"embedded"}).AddSyst(cb,"CMS_looser_lep_wp_etau", "shape", SystMap<>::init(1.00));
 
       cb.cp().process({"embedded"}).AddSyst(cb,"CMS_htt_doublemutrg_2016", "lnN", SystMap<>::init(1.04));
 
